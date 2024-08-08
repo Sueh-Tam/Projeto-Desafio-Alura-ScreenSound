ecdee6f3c1cb72d27e07c4b862ba1fde5159e58e
Matheus Vidal Nunes <matheusvidal2012@gmail.com>
2024-08-08 08:55:32 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public int Nota { get; }
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+}
+
